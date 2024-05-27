48db5f8129087f0e46e149b0998282ceb649da6a
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2024-05-27 21:21:29 +0630

    add route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('First route');
   res.send('respond with a resource');
+  next();
 });
 
+function getByIdHandler(req, res, next) {
+  console.log('get user with id');
+  /*res.json({
+    userId: req.params.userId
+  });*/
+  next();
+}
+
+router.get('/:userId', getByIdHandler,function(req,res,next)
+{
+  console.log('Second handler');
+  res.json({
+    userId: req.params.userId
+  });
+});
+router.get('/one', function(req, res, next) {
+  console.log('user one route');
+  res.send('respond with a resource');
+});
+router.get('/ab?cd', function(req, res, next) {
+  console.log('user /ab?cd');
+  res.send('/ab?cd');
+});
+router.post('/', function(req, res, next) {
+  console.log('Req ',req);
+  res.json({
+    name:'User'
+  });
+});
 module.exports = router;
